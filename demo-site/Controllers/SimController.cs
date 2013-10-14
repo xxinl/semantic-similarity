170ae3669c84d9195b636291cadfbc73fa4b6fa6
Xin <tonyxliu@hotmail.co.uk>
2013-10-14 17:48:57 +0100

    changed to 64 bit build; added edges return from wrapper

--- demo-site/Controllers/SimController.cs
+++ demo-site/Controllers/SimController.cs
@@ -1,38 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Web;
 using System.Web.Http;
 
 namespace demo_site.Controllers
 {
   public class SimController : ApiController
   {
-    [DllImport("sentence-similarity.dll", EntryPoint = "compute_similarity", CallingConvention = CallingConvention.Cdecl)]
-    public static extern float compute_similarity(IntPtr pWne, string s1, string s2);
+    [DllImport("sentence-similarity.dll", EntryPoint = "compute_similarity", CallingConvention = CallingConvention.Cdecl,
+      CharSet = CharSet.Ansi)]
+    public static extern float compute_similarity(IntPtr pWne, string s1, string s2,
+      [Out, MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_BSTR)] out string[] vertices,
+      [Out, MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_BSTR)] out string[] edges);
 
     [DllImport("sentence-similarity.dll", EntryPoint = "compute_similarity2", CallingConvention = CallingConvention.Cdecl)]
     public static extern float compute_similarity2(string s1, string s2);
 
-    [DllImport(@"sentence-similarity.dll", EntryPoint = "connect", CallingConvention = CallingConvention.Cdecl)]
-    public static extern IntPtr connect(string wn_path, string freq_path);
+    [DllImport(@"sentence-similarity.dll", EntryPoint = "connect_wne", CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr connect_wne(string wn_path, string freq_path);
 
-    [DllImport(@"sentence-similarity.dll", EntryPoint = "disconnect", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void disconnect(IntPtr pWne);
+    [DllImport(@"sentence-similarity.dll", EntryPoint = "disconnect_wne", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void disconnect_wne(IntPtr pWne);
 
     // GET api/values
     public string GetSim(string s1, string s2)
     {
-      IntPtr pWne = connect("C:\\Users\\XinL\\Downloads\\libs\\WordNet-3.0\\dict", 
-        "C:\\Projects\\sentence-similarity\\dicts\\freq.txt");
-      float sim = compute_similarity(pWne, s1, s2);
-      //float sim = compute_similarity2("i like that bachelor", "i like that unmarrided man");
+      IntPtr pWne = connect_wne(HttpContext.Current.Server.MapPath(@"~\dicts"),
+        HttpContext.Current.Server.MapPath(@"~\dicts\freq.txt"));
 
+      string[] vertices, edges;
+      float sim = compute_similarity(pWne, s1, s2, out vertices, out edges);
+      //float sim = compute_similarity2(s1, s2);
+      
       return sim.ToString();
     }
-
+    
     // GET api/values/5
     public string Get(int id)
     {
--- msvc/11.0/sentence-similarity/export_wrapper.cpp
+++ msvc/11.0/sentence-similarity/export_wrapper.cpp
@@ -5,14 +5,63 @@
 // Distributed under GPL v2
 //=======================================================================
 
+#include <oaidl.h>
+#include <comutil.h>
+
 #include "sentence_similarity.h"
 
+
 extern "C"	__declspec(dllexport)
-	float compute_similarity(WordnetExtended* pWne, const char* s1, const char* s2)
+	float compute_similarity(WordnetExtended* pWne, const char* s1, const char* s2, SAFEARRAY** vertices, SAFEARRAY** edges)
 {		
 	SentenceSimilarityLi2006 ss(*pWne);
 	WordnetExtended::UndirectedGraph graph;
-	return ss.compute_similarity(s1, s2, graph);
+
+	float sim = ss.compute_similarity(s1, s2, graph);
+	
+	// construct vertices array
+	SAFEARRAY *pV = SafeArrayCreateVector(VT_BSTR, 0, graph.m_vertices.size());
+
+	boost::graph_traits < WordnetExtended::UndirectedGraph >::vertex_iterator vi, vend;
+
+	LONG i = 0;
+	for (boost::tie(vi, vend) = boost::vertices(graph); vi != vend; ++vi) 
+	{
+		int wslen = MultiByteToWideChar(CP_ACP, 0, graph[*vi]->words[0], strlen(graph[*vi]->words[0]), 0, 0);
+		BSTR bstr = SysAllocStringLen(0, wslen);
+		MultiByteToWideChar(CP_ACP, 0, graph[*vi]->words[0], strlen(graph[*vi]->words[0]), bstr, wslen);
+		SafeArrayPutElement(pV, &i, SysAllocString(bstr));
+		SysFreeString(bstr);
+
+		i++;
+	}
+	*vertices = pV;
+
+	*edges = pV;
+
+	// construct edges array
+	SAFEARRAY *pE = SafeArrayCreateVector(VT_BSTR, 0, graph.m_edges.size());
+
+	typedef boost::property_map<WordnetExtended::UndirectedGraph, boost::vertex_index_t>::type VertexIndex;
+	VertexIndex index = get(boost::vertex_index, graph);
+	boost::graph_traits<WordnetExtended::UndirectedGraph>::edge_iterator ei, ei_end;
+
+	i = 0;
+	for (boost::tie(ei, ei_end) = boost::edges(graph); ei != ei_end; ++ei)
+	{		
+		std::string edge = std::to_string(index[source(*ei, graph)]) + "-" + std::to_string(index[target(*ei, graph)]);
+		const char* str = edge.c_str();
+		int wslen = MultiByteToWideChar(CP_ACP, 0, str, strlen(str), 0, 0);
+		BSTR bstr = SysAllocStringLen(0, wslen);
+		MultiByteToWideChar(CP_ACP, 0, str, strlen(str), bstr, wslen);
+		SafeArrayPutElement(pE, &i, SysAllocString(bstr));
+		SysFreeString(bstr);
+
+		i++;
+	}
+	*edges = pE;
+
+	return sim;
 }
 
 extern "C"	__declspec(dllexport)
@@ -22,16 +71,17 @@ extern "C"	__declspec(dllexport)
 		"C:\\Projects\\sentence-similarity\\dicts\\freq.txt");
 	SentenceSimilarityLi2006 ss(wne);
 	WordnetExtended::UndirectedGraph graph;
+
 	return ss.compute_similarity(s1, s2, graph);
 }
 
 extern "C" __declspec(dllexport)
-	WordnetExtended* connect(char* wn_path, char* freq_path) {
+	WordnetExtended* connect_wne(char* wn_path, char* freq_path) {
 		return new WordnetExtended(wn_path, freq_path);
 }
 
 extern "C" __declspec(dllexport)
-	void disconnect(WordnetExtended* pWne) {
+	void disconnect_wne(WordnetExtended* pWne) {
 		delete(pWne);
 }
 
