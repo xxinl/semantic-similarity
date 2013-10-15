be72e00be4b95a20c32fbc9be9c281cec2994824
Xin <tonyxliu@hotmail.co.uk>
2013-10-15 14:04:32 +0100

    added wordnet tree graph - to demo page

---
+++ demo-site/Models/DemoSimResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace demo_site.Models
+{
+  public class DemoSimResult
+  {
+    [JsonProperty("sim")]
+    public float Sim { get; set; }
+    [JsonProperty("vertices")]
+    public string[] Vertices { get; set; }
+    [JsonProperty("edges")]
+    public string[] Edges { get; set; }
+    [JsonProperty("graph")]
+    public GraphNode Graph { get;set; }
+  }
+
+
+  public class GraphNode
+  {
+    [JsonIgnore]
+    public string Index { get; set; }
+    [JsonProperty("name")]
+    public string Words { get; set; }
+    [JsonProperty(PropertyName = "children", NullValueHandling = NullValueHandling.Ignore)]
+    public List<GraphNode> Children { get; set; }
+  }
+}
