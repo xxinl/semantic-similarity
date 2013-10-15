609ad76e022d5def35f7b89c8dbaedbb27f40c1a
Xin <tonyxliu@hotmail.co.uk>
2013-10-15 14:04:32 +0100

    added wordnet tree graph - to demo page

--- demo-site/Controllers/SimController.cs
+++ demo-site/Controllers/SimController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Web;
 using System.Web.Http;
+using demo_site.Models;
 
 namespace demo_site.Controllers
 {
@@ -24,7 +26,7 @@ namespace demo_site.Controllers
     public static extern void disconnect_wne(IntPtr pWne);
 
     // GET api/values
-    public string GetSim(string s1, string s2)
+    public DemoSimResult GetSim(string s1, string s2)
     {
       IntPtr pWne = connect_wne(HttpContext.Current.Server.MapPath(@"~\dicts"),
         HttpContext.Current.Server.MapPath(@"~\dicts\freq.txt"));
@@ -32,29 +34,69 @@ namespace demo_site.Controllers
       string[] vertices, edges;
       float sim = compute_similarity(pWne, s1, s2, out vertices, out edges);
       //float sim = compute_similarity2(s1, s2);
-      
-      return sim.ToString();
-    }
-    
-    // GET api/values/5
-    public string Get(int id)
-    {
-      return "value";
+
+      return new DemoSimResult()
+             {
+               Sim = sim,
+               Graph = GetGraph(vertices, edges),
+               Vertices = vertices,
+               Edges = edges
+             };
     }
 
-    // POST api/values
-    public void Post([FromBody]string value)
+    private GraphNode GetGraph(string[] vertices, string[] edges)
     {
-    }
+      var root = new GraphNode(){Words = "root", Children = new List<GraphNode>()};
+      var nodes = new Dictionary<string, GraphNode>();
+      var ends = new List<GraphNode>();
 
-    // PUT api/values/5
-    public void Put(int id, [FromBody]string value)
-    {
-    }
+      foreach (var edge in edges)
+      {
+        string[] pair = edge.Split('-');
 
-    // DELETE api/values/5
-    public void Delete(int id)
-    {
+        GraphNode source;
+        // TODO here assume souce(pair[0]) doesn't exist in nodes, 
+        //    skip if exist which doesn't quite right if one node have multiple parents
+        if (!nodes.ContainsKey(pair[0]))
+        {
+          source = new GraphNode()
+                   {
+                     Index = pair[0],
+                     Words = vertices[Convert.ToInt32(pair[0])]
+                   };
+          nodes.Add(pair[0], source);
+        }
+        else
+          source = nodes[pair[0]];
+
+        var removeEnd = ends.FirstOrDefault(e => e.Index == pair[0]);
+        if (removeEnd != null)
+          ends.Remove(removeEnd);
+        
+        if (!nodes.ContainsKey(pair[1]))
+        {
+          var target = new GraphNode()
+          {
+            Index = pair[1],
+            Words = vertices[Convert.ToInt32(pair[1])]
+          };
+          nodes.Add(pair[1], target);
+
+          ends.Add(target);
+        }
+
+        if (nodes[pair[1]].Children == null)
+          nodes[pair[1]].Children = new List<GraphNode>();
+
+        nodes[pair[1]].Children.Add(source);
+      }
+
+      foreach (var node in ends)
+      {
+        root.Children.Add(node);
+      }
+
+      return root;
     }
   }
 }
